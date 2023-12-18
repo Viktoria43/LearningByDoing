f297914a8ca539d22b6afc7380bd966e21fd6711
Viktoria43 <145064612+Viktoria43@users.noreply.github.com>
2023-12-18 16:17:11 +0100

    logout page deleted

--- src/pages/Logout.js
+++
@@ -1,5 +0,0 @@
-import Header from "./components/Header";
-import React, {useEffect, useState} from 'react';
-import styled from 'styled-components';
-import axios from "axios";
-import {useNavigate, Link} from "react-router-dom";
