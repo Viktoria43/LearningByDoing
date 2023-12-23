2c15887aae39cc0edc3099d1dd1356c2bd3237ee
cyrusmathew <cyrusmath2002@gmail.com>
2023-12-23 09:04:21 +0100

    content for functions and arrays

--- src/pages/introduction/arrays/Content.js
+++ src/pages/introduction/arrays/Content.js
@@ -1,14 +1,107 @@
 import React from "react";
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { materialDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 const contentStyle = {
     backgroundColor: '#dd3344',
-    height: '500px',
+    height: '1850px',
+    padding: '20px',
 };
 
+const codeBlockStyle = {
+    borderRadius: '15px',
+    overflowX: 'auto',
+    backgroundColor: '#2d2d2d',
+  };
+
 const Content = () => {
+    const codeSnippet = 
+    `// Declaration and Initialization of an array
+    int[] myArray; // Declaration
+    myArray = new int[5]; // Initialization (creating an array of size 5)`;
+
+    const codeSnippet2 = 
+    `// Declaring and Initializing an array in one line
+    int[] numbers = new int[5]; // Creates an array of integers with size 5
+    
+    // Initializing an array with values
+    int[] myArray = { 10, 20, 30, 40, 50 }; // Initializing with specific values`;
+
+    const codeSnippet3 = 
+    `int[] myArray = { 10, 20, 30, 40, 50 };
+
+    // Accessing elements of the array
+    int firstElement = myArray[0]; // Accessing the first element (index 0)
+    int thirdElement = myArray[2]; // Accessing the third element (index 2)
+    
+    // Modifying array elements
+    myArray[4] = 60; // Modifying the fifth element (index 4)`;
+
+    const codeSnippet4 = 
+    `int[] myArray = { 10, 20, 30, 40, 50 };
+
+    // Getting the length of the array
+    int arrayLength = myArray.length; // Returns the length of the array (5 in this case)
+    
+    // Iterating through the array using a for loop
+    for (int i = 0; i < myArray.length; i++) {
+        System.out.println("Element at index " + i + ": " + myArray[i]);
+    }`;
+
+    const codeSnippet5 = 
+    `// Array of Strings
+    String[] names = { "Alice", "Bob", "Charlie" };
+    
+    // Multi-dimensional array
+    int[][] matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };`;
+
     return (
         <div style={contentStyle}>
-            / ---------- Content goes here ----------- /
+            <h1>What are Arrays?</h1>
+            <p>Arrays are a collection of elements of the same type, stored in contiguous memory locations. They provide a convenient way to store and access multiple values under a single variable name.</p>
+            <div style={{ ...codeBlockStyle, maxWidth: '700px' }}>
+            <SyntaxHighlighter language="java" style={materialDark}>
+            {codeSnippet}
+            </SyntaxHighlighter>
+            </div>
+    
+            <h1>Declaring and Initializing Arrays</h1>
+            <p>Arrays can be declared and initialized in Java using square brackets []. Initialization involves allocating memory for the array and specifying its size.</p>
+            <div style={{ ...codeBlockStyle, maxWidth: '750px' }}>
+            <SyntaxHighlighter language="java" style={materialDark}>
+            {codeSnippet2}
+            </SyntaxHighlighter>
+            </div>
+
+            <h1>Accessing Elements of Arrays</h1>
+            <p>Array elements are accessed using their index, starting from 0 to length - 1. Indexing allows reading and modifying individual elements of an array.</p>
+            <p>Arrays can be declared and initialized in Java using square brackets []. Initialization involves allocating memory for the array and specifying its size.</p>
+            <div style={{ ...codeBlockStyle, maxWidth: '700px' }}>
+            <SyntaxHighlighter language="java" style={materialDark}>
+            {codeSnippet3}
+            </SyntaxHighlighter>
+            </div>
+
+            
+            <h1>Array Length and Iterating through Arrays</h1>
+            <p>The length property in Java arrays returns the number of elements in the array. Iterating through arrays using loops (like for loop) helps in accessing and processing each element.</p>
+            <div style={{ ...codeBlockStyle, maxWidth: '850px' }}>
+            <SyntaxHighlighter language="java" style={materialDark}>
+            {codeSnippet4}
+            </SyntaxHighlighter>
+            </div>
+
+            <h1>Arrays of Other Data Types and Multi-Dimensional Arrays</h1>
+            <p>Arrays can hold elements of different data types. Multi-dimensional arrays are arrays of arrays and can have multiple dimensions.</p>
+            <p>The length property in Java arrays returns the number of elements in the array. Iterating through arrays using loops (like for loop) helps in accessing and processing each element.</p>
+            <div style={{ ...codeBlockStyle, maxWidth: '600px' }}>
+            <SyntaxHighlighter language="java" style={materialDark}>
+            {codeSnippet5}
+            </SyntaxHighlighter>
+            </div>
+
+            <h1>Conclusion</h1>
+            <p>Arrays in Java serve as crucial data structures, efficiently storing elements accessed by indices. Their fixed size and flexibility enable diverse applications in programming, ranging from simple data storage to complex algorithmic solutions.</p>
         </div>
     );
 }
--- src/pages/introduction/functions/Content.js
+++ src/pages/introduction/functions/Content.js
@@ -1,14 +1,78 @@
 import React from "react";
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { materialDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 const contentStyle = {
     backgroundColor: '#dd3344',
-    height: '500px',
+    height: '1250px',
+    padding: '20px',
 };
 
+const codeBlockStyle = {
+    borderRadius: '15px',
+    overflowX: 'auto',
+    backgroundColor: '#2d2d2d',
+  };
+
 const Content = () => {
+    const codeSnippet = 
+    `return_type function_name(parameter_list) {
+        // code to be executed
+        return value;
+        }`;
+    
+    
+    const codeSnippet2 = 
+    `public static int add(int num1, int num2) {
+        int sum = num1 + num2;
+        return sum;
+        }`;
+
+
+    const codeSnippet3 = 
+    `public static boolean isEven(int num) {
+        if (num % 2 == 0) {
+        return true;
+        } else {
+        return false;
+            }
+        }`;
+
     return (
         <div style={contentStyle}>
-            / ---------- Content goes here ----------- /
+            <p>Functions are a way to group a set of related statements together that perform a specific task. They are used to break down large programs into smaller, more manageable pieces. In this lesson, we will learn about the basic syntax of functions and how to use them in your code.</p>
+
+            <h1>What are Functions?</h1>
+            <p>Functions are a block of code that performs a specific task. They take input, perform some computation on the input, and return the output. The basic syntax of a function is as follows:</p>
+            <div style={{ ...codeBlockStyle, maxWidth: '650px' }}>
+            <SyntaxHighlighter language="java" style={materialDark}>
+            {codeSnippet}
+            </SyntaxHighlighter>
+            </div>
+            <p>The <span style={{ fontStyle: 'italic' }}>return_type</span> is the data type of the value that the function returns. The <span style={{ fontStyle: 'italic' }}>function_name</span> is the name of the function. The <span style={{ fontStyle: 'italic' }}>parameter_list</span> is a list of parameters that the function takes as input. The value is the output of the function.</p>
+
+            <h1>Examples</h1>
+            <p>Here are some examples of how functions can be used in your code:</p>
+
+            <h3>Example 1: Adding Two Numbers</h3>
+            <div style={{ ...codeBlockStyle, maxWidth: '650px' }}>
+            <SyntaxHighlighter language="java" style={materialDark}>
+            {codeSnippet2}
+            </SyntaxHighlighter>
+            </div>
+            <p>In this example, we are defining a function called <span style={{ fontStyle: 'italic' }}>add</span> that takes two integers as input and returns their sum.</p>
+
+
+            <h3>Example 2: Checking if a Number is Even</h3>
+            <div style={{ ...codeBlockStyle, maxWidth: '650px' }}>
+            <SyntaxHighlighter language="java" style={materialDark}>
+            {codeSnippet3}
+            </SyntaxHighlighter>
+            </div>
+            <p>In this example, we are defining a function called <span style={{ fontStyle: 'italic' }}>isEven</span> that takes an integer as input and returns <span style={{ fontStyle: 'italic' }}>true</span> if it is even and <span style={{ fontStyle: 'italic' }}>false</span> otherwise.</p>
+
+            <h1>Conclusion</h1>
+            <p>Functions are an essential part of programming in Java. By using functions, you can break down large programs into smaller, more manageable pieces. I hope this lesson has helped you understand the basics of functions in Java!</p>
         </div>
     );
 }
